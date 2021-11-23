764cb3ec4bc14211e1a9738e7df12fe9d079b2bd
Maxime Caricand <maxime.caricand@co-assist.fr>
2021-11-23 15:23:38 +0100

    first query

--- graphQL/index.js
+++ graphQL/index.js
@@ -1,15 +1,20 @@
 const { buildSchema } = require('graphql');
+const { getAdByID } = require('../database/services/ad.service');
 
 
 module.exports.schema = buildSchema(`
+    type Ad {
+        id: ID!
+        title: String!
+        price: Int!
+    }
     type Query {
         hello: String
+        ad(id: String!): Ad
     }
 `);
 
 
 module.exports.root = {
-    hello: () => {
-        return 'Hello world!';
-    },
+    ad: async (args) => await getAdByID(args.id)
 };
