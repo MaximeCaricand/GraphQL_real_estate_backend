3b81136878291c4ba69e9a0f617ccc2f6f0abc4f
Maxime Caricand <maxime.caricand@co-assist.fr>
2021-12-02 22:06:25 +0100

    finish setup unit test, Florian fait toi plez

--- test/unit.js
+++ test/unit.js
@@ -8,10 +8,11 @@ const mongoose = require('mongoose');
 describe('Ads Unit Tests', () => {
     describe('Resolvers', () => {
         describe('getAds', () => {
-            it('Should return empty array if no ads found', sinonTest(async function () {
+            it.only('Should return empty array if no ads found', sinonTest(async function () {
                 const findStub = this.stub(mongoose.Model, 'find').resolves([]);
                 const result = await resolvers.Query.getAds();
                 expect(result).to.be.deep.equal([]);
+                expect(findStub.calledOnce).to.be.true;
             }));
             it('Should return empty s', sinonTest(async function () {
                 const result = await resolvers.Query.getAds();
